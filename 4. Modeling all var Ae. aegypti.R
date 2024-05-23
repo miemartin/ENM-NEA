a968baf863ceb4a23ef36df371dab75eb278081d
miemartin <135334498+miemartin@users.noreply.github.com>
2024-05-23 11:31:33 -0300

    4. Modeling all var Ae. aegypti.R

--- 4. Modeling all var Ae. aegypti.R
+++ 4. Modeling all var Ae. aegypti.R
@@ -1,19 +1,14 @@
-library(spocc)
-library(spThin)
-library(dismo)
-library(rgeos)
-library(raster)
-library(rgdal)
-library(ENMeval)
-library(wallace)
-library(sf)
-library(stats)
+# Load libraries
+require(pacman)
+pacman::p_load(spocc, spThin, dismo, raster, rgeos, rgdal, ENMeval, wallace, 
+               sf, stats, SDMtune)
+
 
 ### Analysis for Aedes aegypti (Aa)
 
 # Load occurrence data
 
-occs_path <- "C:/Users/Giga/Desktop/aedes potential maps/South America/current climate/data/"
+occs_path <- "data"
 occs_path <- file.path(occs_path, "wallace_aeg.csv")
 
 # get a list of species occurrence data
@@ -21,24 +16,24 @@ occs_path <- file.path(occs_path, "wallace_aeg.csv")
 userOccs_Aa <- occs_userOccs(
   txtPath = occs_path, 
   txtName = "wallace_aeg.csv", 
-  txtSep = ",", 
+  txtSep = ";", 
   txtDec = ".")
 occs_Aa <- userOccs_Aa$Aedes_aegypti$cleaned
 
 
-### Load environmental data
+# Load environmental data
 
-dir_envs_Aa <- "C:/Users/Giga/Desktop/aedes potential maps/South America/current climate/raster/"
-envs_path <- file.path(dir_envs_Aa, c('bio2.tif', "bio3.tif", "bio8.tif", "bio13.tif", "bio14.tif", "bio18.tif", "bio19.tif", "elevation.tif", 'landcover.tif', 'populationdensity.tif', "urbanaccessibility.tif"))
+dir_envs_Aa <- "raster"
+envs_path <- file.path(dir_envs_Aa, c("bio2.tif", "bio4.tif", "bio8.tif", "bio9.tif", "bio15.tif", "bio17.tif", "bio18.tif", "bio19.tif", "elevation.tif", "land_cover.tif", "population_density.tif", "urban_accessibility.tif"))
 
 # Create environmental object 
 
 envs_Aa <- envs_userEnvs(
   rasPath = envs_path,
-  rasName = c('bio2.tif', "bio3.tif", "bio8.tif", "bio13.tif", "bio14.tif", "bio18.tif", "bio19.tif", "elevation.tif", 'landcover.tif', 'populationdensity.tif', "urbanaccessibility.tif"),
+  rasName = c("bio2.tif", "bio4.tif", "bio8.tif", "bio9.tif", "bio15.tif", "bio17.tif", "bio18.tif", "bio19.tif", "elevation.tif", "land_cover.tif", "population_density.tif", "urban_accessibility.tif"),
   doBrick = FALSE)
 
-occs_xy_Aa <- occs_Aa[c('longitude', 'latitude')]
+occs_xy_Aa <- occs_Aa[c("longitude", "latitude")]
 occs_vals_Aa <- as.data.frame(raster::extract(envs_Aa, occs_xy_Aa, cellnumbers = TRUE))
 
 # remove duplicated same cell values
@@ -59,8 +54,8 @@ occs_vals_Aa <- na.omit(occs_vals_Aa)
 occs_Aa <- cbind(occs_Aa, occs_vals_Aa)
 
 
-### Process environmental data
-#Sampling of 10000 background points and corresponding environmental data using a “point buffers” method with a 2 degree buffer.
+# Process environmental data
+# Sampling of 10000 background points and corresponding environmental data using a “point buffers” method with a 2 degree buffer.
 
 # Generate background extent 
 
@@ -69,12 +64,16 @@ bgExt_Aa <- penvs_bgExtent(
   bgSel = "point buffers",
   bgBuf = 2)
 
+plot(bgExt_Aa)
+
 # Mask environmental data to provided extent
 
 bgMask_Aa <- penvs_bgMask(
   occs = occs_Aa,
   envs = envs_Aa,
   bgExt = bgExt_Aa)
+
+plot(bgMask_Aa)
 
 # Sample background points from the provided area
 
@@ -95,8 +94,8 @@ bgEnvsVals_Aa <- cbind(scientific_name = paste0("bg_", "Aedes aegypti"), bgSampl
                        record_type = NA, bgEnvsVals_Aa)
 
 
-### Partition occurrence data
-#Partition occurrences and background points for model training and validation using random k-fold, a non-spatial partition method.
+# Partition occurrence data
+# Partition occurrences and background points for model training and validation using random k-fold, a non-spatial partition method.
 
 groups_Aa <- part_partitionOccs(
   occs = occs_Aa ,
@@ -105,13 +104,13 @@ groups_Aa <- part_partitionOccs(
   kfolds = 2) 
 
 
-### Build and Evaluate Niche Model
-#Generating a species distribution model using the maxent.jar algorithm as implemented in ENMeval V2.0 (with clamping = TRUE). 
-#For tuning using L, LQ, LQH, LQHP feature classes.
+# Build and Evaluate Niche Model
+# Generating a species distribution model using the maxent.jar algorithm as implemented in ENMeval V2.0 (with clamping = TRUE). 
+# For tuning using L, LQ, LQH, LQHP feature classes.
 
 # Run maxent model for the selected species
 
-# regularization multipliers in the 0.1-1 range increasing by 0.1. 
+# Regularization multipliers in the 0.1-1 range increasing by 0.1. 
 
 model_Aa <- model_maxent(
   occs = occs_Aa,
@@ -123,13 +122,13 @@ model_Aa <- model_maxent(
   fcs = c('L', 'LQ', "H", "LQH", "LQHP"),
   clampSel = TRUE,
   algMaxent = "maxent.jar",
-  catEnvs = "landcover",
+  catEnvs = "land_cover",
   parallel = FALSE)
 
 evalTbl <- model_Aa@results #50 models
-write.csv(evalTbl, "candidate_models_aegypti_all1.csv", row.names=FALSE)
+write.csv(evalTbl, "candidate models/candidate_models_aegypti_all1.csv", row.names=FALSE)
 
-# regularization multipliers in the 2-6 range increasing by 1. 
+# Regularization multipliers in the 2-6 range increasing by 1. 
 
 model_Aa2 <- model_maxent(
   occs = occs_Aa,
@@ -145,9 +144,9 @@ model_Aa2 <- model_maxent(
   parallel = FALSE)
 
 evalTbl2 <- model_Aa2@results #25 models
-write.csv(evalTbl2, "candidate_models_aegypti_all2.csv", row.names=FALSE)
+write.csv(evalTbl2, "candidate models/candidate_models_aegypti_all2.csv", row.names=FALSE)
 
-# regularization multipliers 8 and 10
+# Regularization multipliers 8 and 10
 
 model_Aa3 <- model_maxent(
   occs = occs_Aa,
@@ -163,7 +162,7 @@ model_Aa3 <- model_maxent(
   parallel = FALSE)
 
 evalTbl3 <- model_Aa3@results #10 models
-write.csv(evalTbl3, "candidate_models_aegypti_all3.csv", row.names=FALSE)
+write.csv(evalTbl3, "candidate models/candidate_models_aegypti_all3.csv", row.names=FALSE)
 
 
-### The same code was used for the model that includes only the bioclimatic variables and also for the Ae. albopictus models. 
+# The same code was used for the model that includes only the bioclimatic variables and also for the Ae. albopictus models. 
